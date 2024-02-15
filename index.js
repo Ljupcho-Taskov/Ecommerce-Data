90e3f6fc936fc0984dd3b6eb2358429a769bd08b
Ljupcho.Taskov-FE11 <ljupchotaskov1@gmail.com>
2024-02-15 20:15:58 +0100

    data added

---
+++ index.js
@@ -0,0 +1,14 @@
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 5001;
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
